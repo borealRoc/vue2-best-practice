6a93c7a16b61558413b4dab57b48f655b8e80423
xusp <xspstudyemail@163.com>
2023-06-08 11:07:44 +0800

    chore: 更改本地服务器端口和标题

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
+
+const port = 10086
+const title = 'vue2项目最佳实践'
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port,
+  },
+  configureWebpack: {
+    name: title
+  }
 })
