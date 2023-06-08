00d3eff8611826c8c2e95785a4ba031bf5c78f97
xusp <xspstudyemail@163.com>
2023-06-08 11:23:07 +0800

    feat（创建全局svg-icon组件）：自定义svg-icon组件

---
+++ src/components/svg-icon/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="svg-icon-comp">
+        <svg :class="svgClass">
+            <use :xlink:href="svgName"></use>
+        </svg>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'svg-icon',
+    props: {
+        iconClass: {
+            type: String,
+            default: ''
+        },
+        iconName: {
+            type: String,
+            require: true
+        },
+    },
+    computed: {
+        svgClass() {
+            if (this.iconClass) {
+                return `svg-icon ` + this.iconClass
+            }
+            return `svg-icon`
+        },
+        svgName() {
+            return `#icon-${this.iconName}`
+        }
+    },
+}
+</script>
+  
+<style lang="scss" scoped>
+@import './index.scss'
+</style>
