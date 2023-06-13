0b08888faa24671095fe273d2e7873ecde08d96e
xusp <xspstudyemail@163.com>
2023-06-13 16:28:06 +0800

    fix(全局路由守卫)：解决路由跳转错误

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import constRoutes from './constRoutes'
+
+// 解决路由跳转错误：Uncaught (in promise) Error: Redirected when going from “...“ to “...“ via a navigation guard
+// https://blog.csdn.net/weixin_46873254/article/details/119358833
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location, resolve, reject) {
+  if (resolve || reject) return originalPush.call(this, location, resolve, reject)
+  return originalPush.call(this, location).catch(() => { })
+}
 
 Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: constRoutes
 })
 
 export default router
